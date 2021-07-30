8656f490158cdbdee20c0c1c574fd1fd360c0e81
Aiden gillespie <aiden@cold.org>
2021-07-30 11:37:31 -0600

    Got Password Validator working after looking into readline module

--- password.js
+++ password.js
@@ -1,14 +1,31 @@
-console.log('Welcome, Please input password for validation')
-function verifyPW(){
 
-// }
-function CheckPassword(inputtxt) { 
-let paswd=  /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
-if(inputtxt.value.match(paswd)) 
-    { alert('Correct, try another...')
-    return true;
-} else { 
-    alert('Wrong...!')
-    return false;
-}
-}  
+let readline = require("readline");
+let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+console.log('Welcome to Password Validatinator')
+rl.question("What is your password ? ", function(password) {
+     { if(password.length >= 10){
+         console.log('Great success, long password, welcome back')
+     } else {
+         console.log('Wrong password, it was not validatinated and you will be ejected.')
+     }  
+    };
+    rl.close();
+});
+
+// });
+// rl.question("Welcome, Please input your Password",function() {
+//     if(rl.input.value.length >= 10){
+//         console.log('Success')
+//     } else {
+//         console.log('Wrong Password')
+//     }
+//     rl.close();
+//     });
+
+
+// I love modules, they make things nice, so nice, and it only took and hour and change to find/understand enough about the
+//  readline to get this working
+
